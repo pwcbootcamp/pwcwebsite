a2d8897c64587204a52eacefb0d911c017670f79
Sodiq Farhan <sodiqfarhan360@gmail.com>
2022-07-23 14:30:33 +0100

    Add group 1 project

---
+++ web1/contact.js
@@ -0,0 +1,6 @@
+const contact_button = document.getElementById('button');
+const form = document.getElementById('my-form');
+
+form.onsubmit = function () {
+    window.alert('Thank You! We will reach out to you very soon!')
+}
